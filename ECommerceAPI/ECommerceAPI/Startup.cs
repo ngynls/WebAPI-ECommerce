bf3cab5e8a8c3587f953205135d0f060f261c80d
ngynls <ngyn.ls@gmail.com>
2019-07-31 12:08:58 -0400

    Registered db context to app

--- ECommerceAPI/ECommerceAPI/Startup.cs
+++ ECommerceAPI/ECommerceAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using ECommerceAPI.Models;
 
 namespace ECommerceAPI
 {
@@ -25,6 +27,8 @@ namespace ECommerceAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ShopContext>(opt =>
+              opt.UseInMemoryDatabase("ShopDB"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
