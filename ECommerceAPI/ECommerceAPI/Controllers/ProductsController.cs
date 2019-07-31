0727b4aea08d81641d73c5baa6314b98e2e63d36
ngynls <ngyn.ls@gmail.com>
2019-07-31 11:55:39 -0400

    Basic controller actions for products

---
+++ ECommerceAPI/ECommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetTodoItems()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(long id)
+        {
+            var p = await _context.Products.FindAsync(id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            return p;
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostTodoItem(Product p)
+        {
+            _context.Products.Add(p);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = p.Id }, p);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, Product p)
+        {
+            if (id != p.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(p).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var p = await _context.Products.FindAsync(id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(p);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ECommerceAPI/ECommerceAPI/Models/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Models
+{
+    public class ShopContext:DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
