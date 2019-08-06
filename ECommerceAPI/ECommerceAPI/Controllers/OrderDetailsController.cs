979474122a5996c00290174b0c16ae28fefd9b11
ngynls <ngyn.ls@gmail.com>
2019-08-06 11:34:10 -0400

    Order & OrderDetail Controller

---
+++ ECommerceAPI/ECommerceAPI/Controllers/OrderDetailsController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public OrderDetailsController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrderDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDetail>>> GetOrderDetails()
+        {
+            return await _context.OrderDetails
+                .Include(o => o.Order)
+                .Include(o => o.Product)
+                .ToListAsync();
+        }
+
+        // GET: api/OrderDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDetail>> GetOrderDetail(long id)
+        {
+            _context.ChangeTracker.LazyLoadingEnabled = false;
+            var o = await _context.OrderDetails.FindAsync(id);
+
+            if (o == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(o)
+                .Reference(order => order.Order)
+                .Load();
+
+            _context.Entry(o)
+                .Reference(order => order.Product)
+                .Load();
+
+            return o;
+        }
+
+        // POST: api/OrderDetails
+        [HttpPost]
+        public async Task<ActionResult<OrderDetail>> PostOrderDetail(OrderDetail o)
+        {
+            _context.OrderDetails.Add(o);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrderDetail), new { id = o.Id }, o);
+        }
+
+        // PUT: api/OrderDetails/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderDetails(long id, OrderDetail o)
+        {
+            if (id != o.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(o).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/OrderDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderDetail(long id)
+        {
+            var o = await _context.OrderDetails.FindAsync(id);
+
+            if (o == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderDetails.Remove(o);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ ECommerceAPI/ECommerceAPI/Controllers/OrdersController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public OrdersController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders
+                .Include(c => c.Customer)
+                .ToListAsync();
+        }
+
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(long id)
+        {
+            _context.ChangeTracker.LazyLoadingEnabled = false;
+            var o = await _context.Orders.FindAsync(id);
+
+            if (o == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(o)
+                .Reference(order => order.Customer)
+                .Load();
+
+            return o;
+        }
+
+        // GET: api/Orders/ByCustomer/id
+        [HttpGet("byCustomer/{id}")]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrdersByCustomer(long id)
+        {
+            return await _context.Orders
+                .Where(c => c.CustomerId == id)
+                .ToListAsync();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order o)
+        {
+            _context.Orders.Add(o);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = o.Id }, o);
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(long id, Order o)
+        {
+            if (id != o.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(o).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(long id)
+        {
+            var o = await _context.Orders.FindAsync(id);
+
+            if (o == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(o);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
