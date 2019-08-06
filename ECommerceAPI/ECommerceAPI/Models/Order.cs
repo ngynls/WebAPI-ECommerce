99e72a28e10eaaff8c2d8ecab78515a94ddbdc8c
ngynls <ngyn.ls@gmail.com>
2019-08-05 21:53:20 -0400

    Order & OrderDetail models

---
+++ ECommerceAPI/ECommerceAPI/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public string DateIssued { get; set; }
+        public string ShippedDate { get; set; }
+        public string Status { get; set; }
+        public double Subtotal { get; set; }
+        public double TPSTax { get; set; }
+        public double TVQTax { get; set; }
+        public double Total { get; set; }
+        public string PaymentType { get; set; }
+        public long CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
---
+++ ECommerceAPI/ECommerceAPI/Models/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Models
+{
+    public class OrderDetail
+    {
+        public long Id { get; set; }
+        public long OrderId { get; set; }
+        public long ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
