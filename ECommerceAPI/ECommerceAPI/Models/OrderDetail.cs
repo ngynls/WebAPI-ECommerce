01559c54484245500118c8da730b7607f6ffb91d
ngynls <ngyn.ls@gmail.com>
2019-08-05 21:55:30 -0400

    Nav properties inside OrderDetail

--- ECommerceAPI/ECommerceAPI/Models/OrderDetail.cs
+++ ECommerceAPI/ECommerceAPI/Models/OrderDetail.cs
@@ -11,5 +11,8 @@ namespace ECommerceAPI.Models
         public long OrderId { get; set; }
         public long ProductId { get; set; }
         public int Quantity { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
     }
 }
