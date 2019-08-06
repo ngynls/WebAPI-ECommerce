4c5bddcd1a385727aefce9331f41bcc515c98c60
ngynls <ngyn.ls@gmail.com>
2019-08-05 22:03:09 -0400

    Dbset for Orders & OrderDetails

--- ECommerceAPI/ECommerceAPI/Models/ShopContext.cs
+++ ECommerceAPI/ECommerceAPI/Models/ShopContext.cs
@@ -20,6 +20,8 @@ namespace ECommerceAPI.Models
         public DbSet<CustomerAddress> CustomerAddresses { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
